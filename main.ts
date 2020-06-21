409eb1b2b2b68844bbb3725f8fe9af3fb71e5597
adalbertus <wojciech@pietkiewicz.pl>
2020-06-21 13:02:31 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,44 +6,24 @@ function driveBackward (ms: number) {
         driveStop()
     }
 }
-function driveForward (ms: number) {
-    pins.servoWritePin(AnalogPin.P1, 80)
-    pins.servoWritePin(AnalogPin.P2, 100)
-    
-    basic.pause(50)
-    pins.servoWritePin(AnalogPin.P1, 70)
-    pins.servoWritePin(AnalogPin.P2, 120)
-    
-    basic.pause(30)
-    pins.servoWritePin(AnalogPin.P1, 0)
-    pins.servoWritePin(AnalogPin.P2, 180)
-        
-    // basic.pause(20)
-    // pins.servoWritePin(AnalogPin.P1, 0)
-    if (ms > 0) {
-        basic.pause(ms)
-        driveStop()
-    }
-}
 
-function driveForward2 (ms: number) {
+// go forward slowly - to remove "jumping"
+function driveForward (ms: number) {
     pins.servoWritePin(AnalogPin.P1, 85)
     pins.servoWritePin(AnalogPin.P2, 95)
-    
     basic.pause(50)
+
     pins.servoWritePin(AnalogPin.P1, 80)
     pins.servoWritePin(AnalogPin.P2, 100)
-    
     basic.pause(50)
+
     pins.servoWritePin(AnalogPin.P1, 70)
     pins.servoWritePin(AnalogPin.P2, 120)
-
     basic.pause(30)
+
     pins.servoWritePin(AnalogPin.P1, 0)
     pins.servoWritePin(AnalogPin.P2, 180)
         
-    // basic.pause(20)
-    // pins.servoWritePin(AnalogPin.P1, 0)
     if (ms > 0) {
         basic.pause(ms)
         driveStop()
@@ -55,15 +35,72 @@ function driveStop () {
     pins.servoWritePin(AnalogPin.P2, 90)
 }
 
+function turn(direction : Direction, ms : number = 100) {
+    if(direction == Direction.Right) {
+        pins.servoWritePin(AnalogPin.P1, 90)
+        pins.servoWritePin(AnalogPin.P2, 95)       
+        basic.pause(50)
+
+        pins.servoWritePin(AnalogPin.P1, 90)
+        pins.servoWritePin(AnalogPin.P2, 100)
+        basic.pause(50)
+
+        pins.servoWritePin(AnalogPin.P1, 90)
+        pins.servoWritePin(AnalogPin.P2, 120)
+        basic.pause(30)
+        
+        pins.servoWritePin(AnalogPin.P1, 90)
+        pins.servoWritePin(AnalogPin.P2, 180)
+    } else {
+        pins.servoWritePin(AnalogPin.P1, 85)
+        pins.servoWritePin(AnalogPin.P2, 90)              
+        basic.pause(30)
+
+        pins.servoWritePin(AnalogPin.P1, 60)
+        pins.servoWritePin(AnalogPin.P2, 90)
+        basic.pause(50)
+        
+        pins.servoWritePin(AnalogPin.P1, 0)
+        pins.servoWritePin(AnalogPin.P2, 90)
+        basic.pause(50)
+    }
+    basic.pause(ms)
+    driveStop()
+}
+
+function driveForward2 (ms: number) {
+    pins.servoWritePin(AnalogPin.P1, 80)
+    pins.servoWritePin(AnalogPin.P2, 100)
+    
+    basic.pause(50)
+    pins.servoWritePin(AnalogPin.P1, 70)
+    pins.servoWritePin(AnalogPin.P2, 120)
+    
+    basic.pause(30)
+    pins.servoWritePin(AnalogPin.P1, 0)
+    pins.servoWritePin(AnalogPin.P2, 180)
+        
+    // basic.pause(20)
+    // pins.servoWritePin(AnalogPin.P1, 0)
+    if (ms > 0) {
+        basic.pause(ms)
+        driveStop()
+    }
+}
+
 input.onButtonPressed(Button.A, function () {
     basic.pause(250)
-    driveForward(1000)    
+    //turn(Direction.Right)
+    driveBackward(1000)
 })
 
 input.onButtonPressed(Button.B, function () {
     basic.pause(250)
-    driveForward2(1000)    
+    driveForward(1000)
 })
+
+
+
 basic.forever(function () {
     basic.pause(100)
 })
